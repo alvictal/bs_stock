1ecbfd392e03aad71daceea476f7723f6d9f2c49
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-02-07 08:56:34 -0300

    feat: Adding new stocks and pyarrow as depency of the app

--- bs_stock_batch.sh
+++ bs_stock_batch.sh
@@ -11,3 +11,14 @@ python3 stock_analysis.py TGMA3.SA
 python3 stock_analysis.py NEOE3.SA
 python3 stock_analysis.py SMAL11.SA
 python3 stock_analysis.py VULC3.SA
+python3 stock_analysis.py NGRD3.SA
+python3 stock_analysis.py CXSE3.SA
+python3 stock_analysis.py ALUP11.SA
+python3 stock_analysis.py BPAC11.SA
+python3 stock_analysis.py GMAT3.SA
+python3 stock_analysis.py LEVE3.SA
+python3 stock_analysis.py RAIL3.SA
+python3 stock_analysis.py SUZB3.SA
+python3 stock_analysis.py BBDC4.SA
+python3 stock_analysis.py CMIG4.SA
+python3 stock_analysis.py BRSR6.SA
